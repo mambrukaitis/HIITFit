584012d965bc897503884965aa1fe5b86736d104
Matas Ambrukaitis <mambrukaitis@icloud.com>
2025-01-26 14:36:20 -0500

    Initial Commit

---
+++ Ambrukaitis_COP2654_Mod2HIITFit/Ambrukaitis_COP2654_Mod2HIITFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ambrukaitis_COP2654_Mod2HIITFitApp.swift
+//  Ambrukaitis_COP2654_Mod2HIITFit
+//
+//  Created by Matas Ambrukaitis on 1/26/25.
+//
+
+import SwiftUI
+
+@main
+struct Ambrukaitis_COP2654_Mod2HIITFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
