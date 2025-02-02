c5d4ef1b46a839079f0c1a55526f69dd81c7344b
Matas Ambrukaitis <mambrukaitis@icloud.com>
2025-02-02 13:10:17 -0500

    Added a proper timer.

--- Ambrukaitis_COP2654_Mod2HIITFit/Views/Model/ExerciseView.swift
+++ Ambrukaitis_COP2654_Mod2HIITFit/Views/Model/ExerciseView.swift
@@ -26,10 +26,14 @@ struct ExerciseView: View {
     
     //start and done button functionality
     var startButton: some View {
-        Button("Start Exercise") {}
+        Button("Start Exercise") {
+            showTimer.toggle()
+        }
     }
     var doneButton: some View {
         Button("Done") {
+            timerDone = false
+            showTimer.toggle()
             if lastExercise {
                 showSuccess.toggle()
             } else {
@@ -38,10 +42,11 @@ struct ExerciseView: View {
         }
     }
     
-    let interval: TimeInterval = 30
+    @State private var timerDone = false
+    @State private var showTimer = false
     var body: some View {
         GeometryReader { geometry in
-            VStack {
+            VStack(spacing: 0) {
                 
                 //header
                 HeaderView(
@@ -54,14 +59,11 @@ struct ExerciseView: View {
                 VideoPlayerView(videoName: exercise.videoName)
                     .frame(height: geometry.size.height * 0.45)
                 
-                //timer
-                Text(Date().addingTimeInterval(interval), style: .timer)
-                    .font(.system(size: geometry.size.height * 0.07))
-                
                 //done button
                 HStack(spacing: 150) {
                     startButton
                     doneButton
+                        .disabled(!timerDone)
                         .sheet(isPresented: $showSuccess) {
                             SuccessView(selectedTab: $selectedTab)
                                 .presentationDetents([.medium, .large])
@@ -70,12 +72,20 @@ struct ExerciseView: View {
                 .font(.title3)
                 .padding()
                 
+                //timer
+                if showTimer {
+                    TimerView(
+                        timerDone: $timerDone,
+                        size: geometry.size.height * 0.07
+                        )
+                }
+                Spacer()
+                
                 //rating
                 RatingView(rating: $rating)
                     .padding()
                 
                 //history button
-                Spacer()
                 Button("History") {
                     showHistory.toggle()
                 }
@@ -89,7 +99,7 @@ struct ExerciseView: View {
 }
 
 #Preview {
-    ExerciseView(selectedTab: .constant(1), index: 3)
+    ExerciseView(selectedTab: .constant(3), index: 3)
 }
 
 
